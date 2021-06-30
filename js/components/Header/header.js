88ab910865bb511c79632b8f8a33474aacf97a2a
Alex <powershot317@gmail.com>
2021-06-30 18:28:16 +0300

    done Header component

--- js/components/Header/header.js
+++ js/components/Header/header.js
@@ -1,37 +1,121 @@
 class Header{
     constructor(selector){
-        this.header = document.querySelector(selector);
+        this.headerBlock = document.querySelector(selector);
+        this.selectPage = 'index.html';
+        this.menuList = [
+            {title: 'home', path: 'index.html', icon: 'fas fa-home'},
+            {title: 'about', path: 'about.html', icon: 'fas fa-user'},
+            {title: 'portfolio', path: 'portfolio.html', icon: 'fas fa-suitcase'},
+            {title: 'contact', path: 'contact.html', icon: 'fas fa-envelope-open'},
+        ]
         this.start();
     }
 
     start(){
-        // this.render();
-        this.header.addEventListener('mouseover', this.showTitle.bind(this));
-        this.header.addEventListener('click', this.toggeleMobileMenu.bind(this));
+        let page = location.href.split('/');
+        this.selectPage = page[page.length-1];
+        this.render();
+        this.headerBlock.addEventListener('mouseover', this.showTitle.bind(this));
+        this.headerBlock.addEventListener('click', this.checkTarget.bind(this));
 
     }
 
     render(){
+        let header = document.createElement('header');
+        let desktopMenu = this.createDesktopMenu();
+        let mobileMenu = this.createMobileMenu();
+        
+        header.innerHTML = `
+            ${desktopMenu.outerHTML}
+            ${mobileMenu.outerHTML}
+        `;
+        this.headerBlock.innerHTML = header.outerHTML;
         
     }
 
-    toggeleMobileMenu(e){
-        let myTarget = e.target.closest('.switch-menu__btn');
-        if(myTarget){
-            e.preventDefault();
-            let menuStatus = myTarget.getAttribute('data-status');
-            let mobileMenu = this.header.querySelector('.mobile-menu');
-            if(menuStatus === 'close'){
-                myTarget.innerHTML = '<i class="fas fa-times"></i>';
-                myTarget.setAttribute('data-status', 'open');
+    createDesktopMenu(){
+        let nav = document.createElement('nav');
+        
+        nav.classList.add('desktop-menu');
 
+        let html = this.createLinks();
+        nav.innerHTML = `
+            <ul>
+                ${html}
+            </ul>
+        `;
+
+        return nav;
+
+    }
+
+    createMobileMenu(){
+        let mobileBlock = document.createElement('div');
+        mobileBlock.classList.add('mobile-menu-block');
+        let html = this.createLinks();
+
+        mobileBlock.innerHTML = `
+            <a href="#" class="switch-menu__btn" data-status="close">
+                <i class="fas fa-bars"></i>
+            </a>
+            <nav class="mobile-menu">
+                <ul>
+                    ${html}
+                </ul>
+            </nav>
+        `;
+
+        return mobileBlock;
+    }
+
+    createLinks(){
+        let content = this.menuList.map(item => {
+            let activeEl = '';
+
+            if(this.selectPage === item.path){
+                activeEl = ' ' + 'header-link-active';
             }
-            else{
-                myTarget.innerHTML = '<i class="fas fa-bars"></i>';
-                myTarget.setAttribute('data-status', 'close');
-            }
-            mobileMenu.classList.toggle('mobile-menu-active')
+            
+            return `
+                        <li>
+                            <a href="${item.path}" class="header-link${activeEl}">
+                                <i class="${item.icon} icon"></i>
+                                <span class="nav-title">${item.title}</span>
+                            </a>
+                        </li>
+                    `;
+
+        });
+        return content.join('');
+    }
+
+    checkTarget(e){        
+        if(e.target.closest('.switch-menu__btn')){
+            e.preventDefault()
+            this.toggeleMobileMenu(e.target);
         }
+
+    }
+
+    // redirection(el){
+    //     let myHref = el.getAttribute('href');
+    //     localStorage.setItem('selectPage', myHref)
+    //     // window.location.href = myHref;
+    // }
+
+    toggeleMobileMenu(el){
+        let menuStatus = el.getAttribute('data-status');
+        let mobileMenu = this.headerBlock.querySelector('.mobile-menu');
+        if(menuStatus === 'close'){
+            el.innerHTML = '<i class="fas fa-times"></i>';
+            el.setAttribute('data-status', 'open');
+
+        }
+        else{
+            el.innerHTML = '<i class="fas fa-bars"></i>';
+            el.setAttribute('data-status', 'close');
+        }
+        mobileMenu.classList.toggle('mobile-menu-active');
     }
 
     showTitle(e){    
