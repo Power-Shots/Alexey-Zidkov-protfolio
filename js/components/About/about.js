34b6fb8f6446ccc3afafa3ec403d0bf3c2463897
Alex <powershot317@gmail.com>
2021-07-04 14:29:29 +0300

    start contact.html

--- js/components/About/about.js
+++ js/components/About/about.js
@@ -23,9 +23,10 @@ class AboutMe {
 
     createPersonalInfo(){
         let blockInfo = document.createElement('div');
+        blockInfo.classList.add('info');
         let myInfo = {
             'Имя': 'Алексей',
-            'Адресс': 'Славянск, Донецкая обл.',
+            'Адресс': 'Славянск, Дон. обл.',
             'Фамилия': 'Жидков',
             'Телефон': '+3854452476',
             'Возрост': '20 Лет',
@@ -63,7 +64,7 @@ class AboutMe {
             },
             {
                 count: 3,
-                title: 'Законченных проэктов',
+                title: 'Завершённых проэктов',
             },
             {
                 count: 23,
@@ -90,7 +91,100 @@ class AboutMe {
         return achievementsBlock;
 
     }
+};
+
+
+
+class MeSkills {
+    constructor(block){
+        this.block = document.querySelector(block);
+        this.print();
+    }
+
+    print(){
+        let skillsSection = document.createElement('section');
+        skillsSection.classList.add('me-skills');
+        let skillsBlocks = this.createSkills(); 
+        
+        skillsSection.innerHTML = `
+            <h2>my skills</h2>
+            <div class="skills-block">
+                ${skillsBlocks}
+            </div>
+        `;
+
+        console.log(this.block)
+        this.block.append(skillsSection)
+    }
+
+    createSkills(){
+        let skillsList = [
+            {
+                persent: 90,
+                title: 'html' 
+            },
+            {
+                persent: 85,
+                title: 'css' 
+            },
+            {
+                persent: 70,
+                title: 'js' 
+            },
+            {
+                persent: 80,
+                title: 'git' 
+            },
+            {
+                persent: 50,
+                title: 'jquery' 
+            },
+            {
+                persent: 60,
+                title: 'type script' 
+            },
+            {
+                persent: 70,
+                title: 'wordpress' 
+            },
+            {
+                persent: 10,
+                title: 'angular' 
+            },
+        ];
+
+        let widthAndHeight = 150;
+        let strokeWidth = 7;
+        let radius = (widthAndHeight / 2) - (strokeWidth*2);
+        let circumference = 2 * Math.PI * radius;
+        let content = skillsList.map(item => `
+            <div class="item">
+                <div class="persent-circle-block">
+                    <svg class="progress-ping" width="${widthAndHeight}" height="${widthAndHeight}">
+                        <circle class="progress-ring-circle-bg" stroke-width="${strokeWidth}"
+                        stroke-dasharray="${circumference} ${circumference}"
+                        stroke-dashoffset="${this.setPersent(100, circumference)}"
+                        cx="${widthAndHeight/2}" cy="${widthAndHeight/2}" r="${radius}"/>
+                        <circle class="progress-ring-circle" stroke-width="${strokeWidth}"
+                            stroke-dasharray="${circumference} ${circumference}"
+                            stroke-dashoffset="${this.setPersent(item.persent, circumference)}"
+                            cx="${widthAndHeight/2}" cy="${widthAndHeight/2}" r="${radius}"/>
+                    </svg>
+                    <span class="persent-count">${item.persent}%</span>
+                </div>
+                <p>${item.title}</p>
+            </div>
+        `);
+
+        return content.join('');
+    }
+
+    setPersent(persent, circumference){
+        // console.log(persent, circumference);
+        let offset = circumference - persent / 100 * circumference;
+        return offset;
+    }
 }
 
-
 let aboutMe1 = new AboutMe('#aboutMeBlock');
+let meSkills1 = new MeSkills('#aboutMeBlock');
